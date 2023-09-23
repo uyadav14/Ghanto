2ac9100f42cadddaa9545ca0a1625299b8fd0b49
uyadav14 <68749634+uyadav14@users.noreply.github.com>
2023-09-23 11:30:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -30,7 +30,7 @@ Jᴜsᴛ Tʏᴘᴇ ᴀ Mᴏᴠɪᴇ Nᴀᴍᴇ 🎬**""" )
 
 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅""" )
     UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001832755339")
-    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://postbot:postbot@cluster0.ouwne8q.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://ghantoyadav:<password>@cluster0.7eruxpg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001976028706"))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 20))
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True")
